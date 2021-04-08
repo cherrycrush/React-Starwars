5296e2d26fa7a54820d21b0b37619754410910ca
cherrycrush <phobosnz@gmail.com>
2021-04-08 12:16:35 +1200

    Hello react star wars

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,26 +1,30 @@
 import React, {useState, useEffect} from 'react'
-import {getGreeting} from '../apiClient'
 
 const App = () => {
-
-  const [greeting, setGreeting] = useState('')
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    getGreeting()
-      .then((greeting) => {
-        console.log(greeting)
-        setGreeting(greeting)
-      })
-  }, [count])
-
   return (
-    <>
-    {count}
-    <h1>{greeting}</h1>
-    <button onClick={() => setCount(count + 1)}>Click</button>
-    </>
+    <div>
+      <h1>Hello react star wars</h1>
+    </div>
   )
 }
 
 export default App
+
+// const [greeting, setGreeting] = useState('')
+// const [count, setCount] = useState(0)
+
+// useEffect(() => {
+//   getGreeting()
+//     .then((greeting) => {
+//       console.log(greeting)
+//       setGreeting(greeting)
+//     })
+// }, [count])
+
+// return (
+//   <>
+//     {count}
+//     <h1>{greeting}</h1>
+//     <button onClick={() => setCount(count + 1)}>Click</button>
+//   </>
+// )
