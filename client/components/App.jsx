ae82d2d461d1e62685001f41f4876c97a9054058
cherrycrush <phobosnz@gmail.com>
2021-04-10 17:56:47 +1200

    Remove comment of counter

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -12,22 +12,3 @@ const App = () => {
 }
 
 export default App
-
-// const [greeting, setGreeting] = useState('')
-// const [count, setCount] = useState(0)
-
-// useEffect(() => {
-//   getGreeting()
-//     .then((greeting) => {
-//       console.log(greeting)
-//       setGreeting(greeting)
-//     })
-// }, [count])
-
-// return (
-//   <>
-//     {count}
-//     <h1>{greeting}</h1>
-//     <button onClick={() => setCount(count + 1)}>Click</button>
-//   </>
-// )
